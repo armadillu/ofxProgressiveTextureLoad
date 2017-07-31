ec95c1cac13c17876383104bb5a58afbb52d2602
Oriol Ferrer Mesia <oriol@uri.cat>
2017-07-31 12:55:20 -0400

    mroe appveyor tests

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Executing ci/linux/install.sh"
+echo "Executing ci/vs/install.sh"
 
-export OF_ROOT=~/openFrameworks
+export OF_ROOT="$APPVEYOR_BUILD_FOLDER/openFrameworks"
 export OF_ADDONS=$OF_ROOT/addons
 
 ADDONS="armadillu/ofxHistoryPlot armadillu/ofxTimeMeasurements"
